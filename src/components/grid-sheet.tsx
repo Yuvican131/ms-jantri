0b8160f462863cf8bf25842cf8ed8861426eb7de
Yuvraj Singh <yuvrajsaini131@gmail.com>
2025-08-23 19:53:28 +0000

    make client box small rectangular

--- src/components/grid-sheet.tsx
+++ src/components/grid-sheet.tsx
@@ -713,7 +713,7 @@ const handleHarupApply = () => {
                             Master Sheet
                         </Button>
                     </div>
-                    <div className="border rounded-lg p-2 flex flex-col gap-2 h-24 w-24">
+                    <div className="border rounded-lg p-2 flex flex-col gap-2 h-24 w-32">
                         <h3 className="font-semibold text-center">Client</h3>
                     </div>
                 </div>
