3e4f19deca1f3c87ef6608f91d105c809a09f408
Yuvraj Singh <yuvrajsaini131@gmail.com>
2026-02-01 15:04:26 +0000

    i want to connect my firebase database to my netlify app that is built u

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
-  "projectId": "ms-jantri-55079957-98b95",
-  "appId": "1:114108861217:web:c3d391d66e156b570c0c60",
-  "apiKey": "AIzaSyDKxGOWyVi6GXpfauTRLaGPGFREkZa-epg",
-  "authDomain": "ms-jantri-55079957-98b95.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "114108861217"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
