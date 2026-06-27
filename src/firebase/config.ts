372b4ccdf3636e44e79db33cb5728ea0cc530b53
Yuvraj <s1812@yuvraj.com>
2026-06-27 15:25:20 +0530

    fix storageBucket to match Firebase Console

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -31,7 +31,7 @@ const hardcodedConfig = {
   apiKey: "AIzaSyDKxGOWyVi6GXpfauTRLaGPGFREkZa-epg",
   authDomain: "ms-jantri-55079957-98b95.firebaseapp.com",
   projectId: "ms-jantri-55079957-98b95",
-  storageBucket: "ms-jantri-55079957-98b95.appspot.com", // Corrected domain for storage bucket
+  storageBucket: "ms-jantri-55079957-98b95.firebasestorage.app", // Updated to match Firebase Console
   messagingSenderId: "114108861217",
   appId: "1:114108861217:web:c3d391d66e156b570c0c60",
   measurementId: "",
